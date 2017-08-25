1114571930cb064ccd2c09a203066445599741ef
Zhe Liu <zl376@cornell.edu>
2017-08-25 10:07:24 -0400

    Modify:	1. Trim with support for matrix size (1x3)

--- trim3.m
+++ trim3.m
@@ -2,6 +2,9 @@ function res = trim3(img, input_arg)
 
 if size(input_arg,1) == 1 
     idxs_crop = input_arg;
+    if size(input_arg,2) == 3
+        idxs_crop = [1, input_arg(1), 1, input_arg(2), 1, input_arg(3)];
+    end
 elseif size(input_arg,2) == 1
     idxs_crop = input_arg;
     idxs_crop = [1, idxs_crop(2)-idxs_crop(1)+1, 1, idxs_crop(4)-idxs_crop(3)+1, 1, idxs_crop(6)-idxs_crop(5)+1];
