ca5964e83e61b25c4647588d26707afb6dfc1d80
Zhe Liu <zl376@cornell.edu>
2018-04-19 21:54:51 -0400

    Modify:   1. Consistent result when generating idxs_crop from Mask (between trim3.m and get_idxs_crop.m)

--- get_idxs_crop.m
+++ get_idxs_crop.m
@@ -1,7 +1,11 @@
 % Note:
 %   Length should be EVEN number along each dimension
 
-function [idxs_crop] = get_idxs_crop(Mask, margin)
+function [idxs_crop] = get_idxs_crop(Mask, margin, flag_checkodd)
+
+if nargin < 3
+    flag_checkodd = 1;
+end
 
 if nargin < 2
     margin = 0;
@@ -22,31 +26,33 @@ d3first = max(find(d3,1,'first')-margin, 1);
 d3last = min(find(d3,1,'last')+margin, matrix_size(3));
 
 % Check if odd dimension
-if mod(d1last - d1first + 1, 2) == 1
-    if d1first > 1
-        d1first = d1first - 1;
-    elseif d1last < matrix_size(1)
-        d1last = d1last + 1;
-    else
-        error('Dimension 1 must have even length.');
+if flag_checkodd
+    if mod(d1last - d1first + 1, 2) == 1
+        if d1first > 1
+            d1first = d1first - 1;
+        elseif d1last < matrix_size(1)
+            d1last = d1last + 1;
+        else
+            error('Dimension 1 must have even length.');
+        end
     end
-end
-if mod(d2last - d2first + 1, 2) == 1
-    if d2first > 1
-        d2first = d2first - 1;
-    elseif d2last < matrix_size(2)
-        d2last = d2last + 1;
-    else
-        error('Dimension 2 must have even length.');
+    if mod(d2last - d2first + 1, 2) == 1
+        if d2first > 1
+            d2first = d2first - 1;
+        elseif d2last < matrix_size(2)
+            d2last = d2last + 1;
+        else
+            error('Dimension 2 must have even length.');
+        end
     end
-end
-if mod(d3last - d3first + 1, 2) == 1
-    if d3first > 1
-        d3first = d3first - 1;
-    elseif d3last < matrix_size(3)
-        d3last = d3last + 1;
-    else
-        error('Dimension 3 must have even length.');
+    if mod(d3last - d3first + 1, 2) == 1
+        if d3first > 1
+            d3first = d3first - 1;
+        elseif d3last < matrix_size(3)
+            d3last = d3last + 1;
+        else
+            error('Dimension 3 must have even length.');
+        end
     end
 end
 
--- trim3.m
+++ trim3.m
@@ -11,20 +11,7 @@ elseif size(input_arg,2) == 1
 else
     Mask = input_arg;
 
-    matrix_size0 = size(img);
-    d1 = max(max(Mask,[],2),[],3);
-    d1first = find(d1,1,'first');
-    d1last = find(d1,1,'last');
-
-    d2 = max(max(Mask,[],1),[],3);
-    d2first = find(d2,1,'first');
-    d2last = find(d2,1,'last');
-
-    d3 = max(max(Mask,[],1),[],2);
-    d3first = find(d3,1,'first');
-    d3last = find(d3,1,'last');
-
-    idxs_crop = [d1first, d1last, d2first, d2last, d3first, d3last]
+    idxs_crop = get_idxs_crop(Mask);
 end
 
 res = img(idxs_crop(1):idxs_crop(2), idxs_crop(3):idxs_crop(4), idxs_crop(5):idxs_crop(6), :);
